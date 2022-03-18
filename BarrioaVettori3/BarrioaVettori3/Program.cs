9572ac4770324d0fc00cdcbe5c0ca0494f64a458
marialuisabarrios <98150919+marialuisabarrios@users.noreply.github.com>
2022-03-18 10:44:58 +0100

    es 2 e3

---
+++ BarrioaVettori3/BarrioaVettori3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BarrioaVettori3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crea una app che carica un vettore di 30 elementi con valori casuali
+            //compresi tra 0 e 50 e stampi la media dei valori contenuti nel vettore
+            const int dim = 50;
+            int[] numeri = new int[30];
+            Random generatore = new Random();
+            for (int i = 0; i < dim; i++)
+            {
+                numeri[i] = generatore.Next(0, 50);
+            }
+            for (int i = 0; i < dim; i++)
+            {
+                Console.WriteLine(numeri[i]);
+            }
+         
+        }
+    }
+}
